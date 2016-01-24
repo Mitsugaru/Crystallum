69703e83598933a7b8c5472aee98de0a5348d7da
Vincent Deloso <vincentdeloso@gmail.com>
2016-01-24 09:00:22 -0700

    Added test for XXHash.

---
+++ Assets/Editor/Tests/xxHashTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+[TestFixture]
+public class xxHashTest
+{
+    private static readonly Random random = new Random();
+    private int seed;
+    private HashFunction function;
+    [SetUp]
+    public void Init()
+    {
+        seed = random.Next(int.MinValue, int.MaxValue);
+        function = new XXHash(seed);
+    }
+
+    [Test]
+    public void SameSeedSameNumbers()
+    {
+        HashFunction duplicate = new XXHash(seed);
+        for (int i = 0; i < 100; i++)
+        {
+            int key = random.Next(int.MinValue, int.MaxValue);
+            Assert.AreEqual(function.Value(key), duplicate.Value(key), "Hash functions of same seed given same value should produce same output.");
+        }
+    }
+}
