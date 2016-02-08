f4eb70420258ed872becdd28e6f661fc44226e6a
Vincent Deloso <mitsugaru@gmail.com>
2016-02-07 19:38:41 -0700

    Added simple UI toggle script for buttons. Added entity generator as seen from the entity manager, for later use.

---
+++ Assets/Scripts/Player/EntityGenerator.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityGenerator : MonoBehaviour
+{
+
+    public int seed = 0;
+    protected int currentSeed = 0;
+
+    public int start = 0;
+
+    protected HashFunction random;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentSeed = seed;
+        random = new XXHash(currentSeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (seed != currentSeed)
+        {
+            currentSeed = seed;
+            random = new XXHash(currentSeed);
+        }
+    }
+
+    public Entity Generate()
+    {
+        return new Entity(unchecked((int)random.GetHash(start++)));
+    }
+}
---
+++ Assets/Scripts/UI/ToggleUIScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleUIScript : MonoBehaviour
+{
+
+    public GameObject Target;
+
+    // Use this for initialization
+    void Start()
+    {
+        Button button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(Toggle);
+    }
+
+    public void Toggle()
+    {
+        Target.SetActive(!Target.activeInHierarchy);
+    }
+}
