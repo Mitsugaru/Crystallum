b2aa64fd87ee60cee37f97e053976381d0192189
Vincent Deloso <mitsugaru@gmail.com>
2016-02-10 13:13:45 -0700

    Added missing name debug script.

---
+++ Assets/Scripts/UI/NameDebug.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using strange.extensions.mediation.impl;
+
+public class NameDebug : View
+{
+
+    [Inject]
+    public INameGenerator NameGenerator { get; set; }
+
+    public void OutputName()
+    {
+        Debug.Log(NameGenerator.GenerateName());
+    }
+}
