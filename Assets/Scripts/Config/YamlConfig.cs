98da7eae336f0b4d5a47100c0d66c604c6e3506c
Vincent Deloso <mitsugaru@gmail.com>
2016-02-10 13:20:11 -0700

    Added base yaml config class.

---
+++ Assets/Scripts/Config/YamlConfig.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using YamlDotNet.Serialization;
+
+public class YamlConfig {
+
+    protected string path;
+
+    public YamlConfig(string path)
+    {
+        this.path = path;
+    }
+
+    protected T Parse<T>()
+    {
+        T conf = default(T);
+        if(File.Exists(path))
+        {
+            using(StreamReader reader = new StreamReader(path))
+            {
+                Deserializer deserializer = new Deserializer(ignoreUnmatched: true);
+                conf = deserializer.Deserialize<T>(reader);
+            }
+        }
+        return conf;
+    }
+
+    protected void Write(object source)
+    {
+        using(TextWriter writer = File.CreateText(path))
+        {
+            Serializer serializer = new Serializer();
+            serializer.Serialize(writer, source);
+        }
+    }
+}
