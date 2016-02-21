b6c1076da95314a780fbf51dc8230a7a603b2469
Vincent Deloso <mitsugaru@gmail.com>
2016-02-20 21:44:39 -0700

    Changed how the battle ui manager works with buttons.

--- Assets/Scripts/UI/BattleUIManager.cs
+++ Assets/Scripts/UI/BattleUIManager.cs
@@ -8,6 +8,9 @@ public class BattleUIManager : View
 {
 
     [Inject]
+    public IEventManager EventManager { get; set; }
+
+    [Inject]
     public IEntityManager EntityManager { get; set; }
 
     [Inject]
@@ -19,33 +22,48 @@ public class BattleUIManager : View
 
     public Transform playerPartyPanel;
 
-    public Button enemyGenerateButton;
+    public GameObject enemyGenerateButton;
 
-    public Button enemyClearButton;
+    public GameObject enemyClearButton;
 
-    public Button playerGenerateButton;
+    public GameObject playerGenerateButton;
 
-    public Button playerClearButton;
+    public GameObject playerClearButton;
+
+    public GameObject configButton;
+
+    public GameObject battleButton;
+
+    public GameObject commandPanel;
 
     public BattleConfig EnemyParty;
 
     public BattleConfig PlayerParty;
+
+    private Dictionary<Entity, CharacterPanelScript> panelScripts = new Dictionary<Entity, CharacterPanelScript>();
 
     // Use this for initialization
     protected override void Start()
     {
         base.Start();
 
-        enemyGenerateButton.onClick.AddListener(HandleEnemyGenerate);
-        enemyClearButton.onClick.AddListener(HandleEnemyClear);
-        playerGenerateButton.onClick.AddListener(HandlePlayerGenerate);
-        playerClearButton.onClick.AddListener(HandlePlayerClear);
+        configButton.GetComponent<Button>().onClick.AddListener(HandleConfig);
+        enemyGenerateButton.GetComponent<Button>().onClick.AddListener(HandleEnemyGenerate);
+        enemyClearButton.GetComponent<Button>().onClick.AddListener(HandleEnemyClear);
+        playerGenerateButton.GetComponent<Button>().onClick.AddListener(HandlePlayerGenerate);
+        playerClearButton.GetComponent<Button>().onClick.AddListener(HandlePlayerClear);
+        battleButton.GetComponent<Button>().onClick.AddListener(HandleBattleButton);
     }
 
     // Update is called once per frame
     void Update()
     {
+        battleButton.GetComponent<Button>().interactable = BattleManager.EnemyParty.Count > 0 && BattleManager.PlayerParty.Count > 0;
+    }
 
+    private void HandleConfig()
+    {
+        commandPanel.SetActive(!commandPanel.activeInHierarchy);
     }
 
     private void HandleEnemyGenerate()
@@ -53,7 +71,7 @@ public class BattleUIManager : View
         if (EnemyParty.partySize > 0)
         {
             StartCoroutine(GenerateEnemyParty());
-            enemyGenerateButton.interactable = false;
+            enemyGenerateButton.GetComponent<Button>().interactable = false;
         }
     }
 
@@ -65,14 +83,18 @@ public class BattleUIManager : View
         foreach (Transform child in enemyPartyPanel)
         {
             children.Add(child.gameObject);
+            CharacterPanelScript script = child.gameObject.GetComponent<CharacterPanelScript>();
+            if (script != null)
+            {
+                panelScripts.Remove(script.Character);
+            }
         }
         children.ForEach(child => Destroy(child));
-        enemyGenerateButton.interactable = true;
+        enemyGenerateButton.GetComponent<Button>().interactable = true;
     }
 
     private IEnumerator GenerateEnemyParty()
     {
-        Debug.Log(EnemyParty.partySize);
         for (int i = 0; i < EnemyParty.partySize; i++)
         {
             Entity entity = EntityManager.Generate();
@@ -82,8 +104,10 @@ public class BattleUIManager : View
             if (script != null)
             {
                 script.Character = entity;
+                panelScripts.Add(entity, script);
             }
             charPanel.transform.SetParent(enemyPartyPanel);
+            charPanel.name = entity.Name + " Panel";
             yield return null;
         }
     }
@@ -93,7 +117,7 @@ public class BattleUIManager : View
         if (PlayerParty.partySize > 0)
         {
             StartCoroutine(GeneratePlayerParty());
-            playerGenerateButton.interactable = false;
+            playerGenerateButton.GetComponent<Button>().interactable = false;
         }
     }
 
@@ -108,8 +132,10 @@ public class BattleUIManager : View
             if (script != null)
             {
                 script.Character = entity;
+                panelScripts.Add(entity, script);
             }
             charPanel.transform.SetParent(playerPartyPanel);
+            charPanel.name = entity.Name + " Panel";
             yield return null;
         }
         yield return null;
@@ -123,9 +149,33 @@ public class BattleUIManager : View
         foreach (Transform child in playerPartyPanel)
         {
             children.Add(child.gameObject);
+            CharacterPanelScript script = child.gameObject.GetComponent<CharacterPanelScript>();
+            if (script != null)
+            {
+                panelScripts.Remove(script.Character);
+            }
         }
         children.ForEach(child => Destroy(child));
-        playerGenerateButton.interactable = true;
+        playerGenerateButton.GetComponent<Button>().interactable = true;
     }
 
+    private void HandleBattleButton()
+    {
+        configButton.GetComponent<Button>().interactable = false;
+        enemyGenerateButton.GetComponent<Button>().interactable = false;
+        enemyClearButton.GetComponent<Button>().interactable = false;
+        playerGenerateButton.GetComponent<Button>().interactable = false;
+        playerClearButton.GetComponent<Button>().interactable = false;
+        commandPanel.SetActive(false);
+        //TODO tell battle manager to start a battle
+        if (BattleManager.InBattle())
+        {
+            BattleManager.StepBattle();
+        }
+        else
+        {
+            BattleManager.StartBattle();
+            battleButton.GetComponentInChildren<Text>().text = "Step Battle";
+        }
+    }
 }
