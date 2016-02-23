cb099d194ecec041c174b0b1e314aa79eb7291eb
Vincent Deloso <mitsugaru@gmail.com>
2016-02-22 23:45:07 -0700

    Added BattleActorInfo holder class.

---
+++ Assets/Scripts/Battle/BattleActorInfo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleActorInfo
+{
+
+    protected BattleStats stats;
+    public BattleStats Stats
+    {
+        get
+        {
+            return stats;
+        }
+    }
+
+    protected int value = 0;
+    public int Value
+    {
+        get
+        {
+            return value;
+        }
+    }
+
+    protected int level = 0;
+    public int Level
+    {
+        get
+        {
+            return level;
+        }
+    }
+
+    public BattleActorInfo(BattleStats stats, int value, int level)
+    {
+        this.stats = stats;
+        this.value = value;
+        this.level = level;
+    }
+}
