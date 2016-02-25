cfc708d6de7c15e153c37bf0e03586234d88ca86
Vincent Deloso <mitsugaru@gmail.com>
2016-02-24 23:06:39 -0700

    Added logarithmic and normal exponential battle formulas.

---
+++ Assets/Scripts/Formulas/Battle/LogarithmicBattleFormula.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class LogarithmicBattleFormula : AbstractBattleFormula<int>
+{
+    public override int Generate(BattleActorInfo attacker, BattleActorInfo defender, int turn, int actionCount)
+    {
+        int maxStat = StatUtils.CalcMaxStat(attacker.Level);
+        int modifier = Mathf.Abs(random.Range(Multiplier.Minimum, Multiplier.Maximum, turn, actionCount) - (maxStat - attacker.Value) / maxStat) * maxStat;
+        int dmg = (int)(Mathf.Log10(attacker.Value) / Mathf.Log10(maxStat) * maxStat + modifier);
+        return ApplyDefense(dmg, defender, turn, actionCount);
+    }
+}
---
+++ Assets/Scripts/Formulas/Battle/NormalExponentialBattleFormula.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class NormalExponentialBattleFormula : AbstractBattleFormula<int>
+{
+    public override int Generate(BattleActorInfo attacker, BattleActorInfo defender, int turn, int actionCount)
+    {
+        int dmg = (int)(attacker.Value * Mathf.Pow(random.Range(Multiplier.Minimum, Multiplier.Maximum), StatUtils.CalcEValue(attacker.Level, attacker.Stats.Limit)));
+        return ApplyDefense(dmg, defender, turn, actionCount);
+    }
+}
